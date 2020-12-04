85af1ae9f333fb6e88ebe812f5c3f016fe15cd50
Guillermo Muntaner <guillermomp87@gmail.com>
2020-12-04 20:23:17 +0100

    Read about testing and added some tests for day 2 examples

--- src/day2.rs
+++ src/day2.rs
@@ -46,10 +46,29 @@ fn parse_line(line: &str) -> (i32, i32, char, &str) {
     return (min, max, char, &password);
 }
 
+#[test] pub fn test_parse_line() {
+    let (min, max, char, password) = parse_line(&"1-3 a: abcde");
+    assert_eq!(min, 1);
+    assert_eq!(max, 3);
+    assert_eq!(char, 'a');
+    assert_eq!(password, "abcde");
+}
+
 fn validate(data: &(i32, i32, char, &str)) -> bool {
     let (min, max, char, password) = data;
     let occurrences = password.matches(*char).count() as i32;
     return occurrences >= *min && occurrences <= *max;
+}
+
+#[test] pub fn test_validate() {
+    // Valid examples:
+    // 1-3 a: abcde
+    // 2-9 c: ccccccccc
+    assert_eq!(validate(&(1, 3, 'a', "abcde")), true);
+    assert_eq!(validate(&(2, 9, 'c', "ccccccccc")), true);
+    // Invalid example:
+    // 1-3 b: cdefg
+    assert_eq!(validate(&(1, 3, 'b', "cdefg")), false);
 }
 
 pub fn count_valid_passwords(lines: &Vec<String>) -> usize {
@@ -58,13 +77,6 @@ pub fn count_valid_passwords(lines: &Vec<String>) -> usize {
         .filter(|data| validate(data))
         .count()
 }
-
-// TODO: Testing??
-pub fn test() {
-    parse_line(&"1-3 a: abcde");
-    println!("Is valid example: {}", validate(&(1, 3, 'a', "abcde")));
-}
-
 
 // --- Part Two ---
 // While it appears you validated the passwords correctly, they don't seem to be what the Official
@@ -91,6 +103,15 @@ fn validate_new_rules(data: &(i32, i32, char, &str)) -> bool {
     let first = password.chars().nth(*min as usize - 1).unwrap();
     let second = password.chars().nth(*max as usize - 1).unwrap();
     return vec![first, second].iter().filter(|&c| c == char).count() == 1
+}
+
+#[test] pub fn test_validate_new_rules() {
+    // 1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+    assert_eq!(validate_new_rules(&(1, 3, 'a', "abcde")), true);
+    // 1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+    assert_eq!(validate_new_rules(&(1, 3, 'b', "cdefg")), false);
+    // 2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+    assert_eq!(validate_new_rules(&(1, 3, 'a', "ccccccccc")), false);
 }
 
 pub fn count_valid_passwords_new_rules(lines: &Vec<String>) -> usize {
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ fn main() {
     println!("Day 1 - Part 2: {}", day1::part2(&day1_input));
 
     let day2_input = input_utils::read_all("inputs/day2");
-    day2::test();
     println!("Day 2 - Part 1: {}", day2::count_valid_passwords(&day2_input));
     println!("Day 2 - Part 1: {}", day2::count_valid_passwords_new_rules(&day2_input));
 }

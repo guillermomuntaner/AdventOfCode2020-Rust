e9d348c44b3ffd35cf487d083b2d4ed98291bc4e
Guillermo Muntaner <guillermomp87@gmail.com>
2020-12-08 12:28:21 +0100

    Added tests for all day solutions to avoid regressions refactoring.

--- src/main.rs
+++ src/main.rs
@@ -41,3 +41,66 @@ fn main() {
     println!("Day 8 - Part 1: {}", day8::accumulator_value_before_entering_loop(&day8_input));
     println!("Day 8 - Part 2: {}", day8::accumulator_value_fixing_loop(&day8_input));
 }
+
+
+/// Test all previous results stay stable.
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn test_day1() {
+        let day1_input = input_utils::read_all_as::<u32>("inputs/day1");
+        assert_eq!(day1::part1(&day1_input), 877971);
+        assert_eq!(day1::part2(&day1_input), 203481432);
+    }
+
+    #[test]
+    pub fn test_day2() {
+        let day2_input = input_utils::read_all("inputs/day2");
+        assert_eq!(day2::count_valid_passwords(&day2_input), 548);
+        assert_eq!(day2::count_valid_passwords_new_rules(&day2_input), 502);
+    }
+
+    #[test]
+    pub fn test_day3() {
+        let day3_input = input_utils::read_all("inputs/day3");
+        assert_eq!(day3::calculate_cut_trees_for_cheap_toboggan(&day3_input), 259);
+        assert_eq!(day3::calculate_slopes_cost_multiplied(&day3_input), 2224913600);
+    }
+
+    #[test]
+    pub fn test_day4() {
+        let day4_input = input_utils::read_all("inputs/day4");
+        assert_eq!(day4::count_passwords_with_all_fields(&day4_input), 235);
+        assert_eq!(day4::count_valid_passwords(&day4_input), 194);
+    }
+
+    #[test]
+    pub fn test_day5() {
+        let day5_input = input_utils::read_all("inputs/day5");
+        assert_eq!(day5::find_highest_id(&day5_input), 828);
+        assert_eq!(day5::find_seat_id(&day5_input), 565);
+    }
+
+    #[test]
+    pub fn test_day6() {
+        let day6_input = input_utils::read_all("inputs/day6");
+        assert_eq!(day6::count_number_of_unique_group_yes_answers(&day6_input), 6878);
+        assert_eq!(day6::count_number_of_unanimous_group_yes_answers(&day6_input), 3464);
+    }
+
+    #[test]
+    pub fn test_day7() {
+        let day7_input = input_utils::read_all("inputs/day7");
+        assert_eq!(day7::count_bags_containing_shiny_gold(&day7_input), 326);
+        assert_eq!(day7::count_bags_inside_shiny_gold(&day7_input), 5635);
+    }
+
+    #[test]
+    pub fn test_day8() {
+        let day8_input = input_utils::read_all("inputs/day8");
+        assert_eq!(day8::accumulator_value_before_entering_loop(&day8_input), 1521);
+        assert_eq!(day8::accumulator_value_fixing_loop(&day8_input), 1016);
+    }
+}
